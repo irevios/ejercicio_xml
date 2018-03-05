67264a173f647af1a697629891d8c32987fccb38
irevios <irevios@gmail.com>
2018-03-05 16:01:53 +0100

    Tercer apartado

--- Cursos XML.py
+++ Cursos XML.py
@@ -6,9 +6,8 @@ def listar_nomb_tema_fechaini( arbol ):
 	nombres = arbol.xpath( '//curso/nombre/text()' )
 	for i in nombres:
 		temas = arbol.xpath( '//curso[nombre="'+i+'"]/temas/tema/text()' )
-		fechaini = arbol.xpath( '//curso[nombre="'+i+'"]/fechainicio/text()' )
-		i = i.split( '\n\t\t\t' )[1].split( '\n' )[0]
-		curso = ( i, temas, fechaini[0] )
+		fechaini = arbol.xpath( '//curso[nombre="'+i+'"]/fechainicio/text()' )[0]
+		curso = ( i.split( '\n\t\t\t' )[1].split( '\n' )[0], temas, fechaini )
 		lista.append( curso )
 	
 	return lista
@@ -17,4 +16,15 @@ def curso_por_tema_perfil(tema,perfil,arbol):
 	cursos = arbol.xpath('//curso[temas/tema="'+tema+'" and perfiles/perfil="'+perfil+'"]')
 	return len(cursos)
 
-print(curso_por_tema_perfil('Educación','Adultos',arbol))
+def fecha_por_mes(mes,arbol):
+	lista = []
+	nombres = arbol.xpath('//curso/nombre/text()')
+	for i in nombres:
+		fechaini = arbol.xpath( '//curso[nombre="'+i+'"]/fechainicio/text()' )[0]
+		m = fechaini.split('/')[1]
+		if int(m) == mes:
+			curso = (i.split( '\n\t\t\t' )[1].split( '\n' )[0],fechaini)
+			lista.append(curso)
+	return lista
+
+print(fecha_por_mes(10,arbol))
