5252f53d03ae397b3064c297d301c4466f74f942
irevios <irevios@gmail.com>
2018-03-05 16:08:55 +0100

    Validadores de datos introducidos por usuario

--- Cursos XML.py
+++ Cursos XML.py
@@ -44,18 +44,15 @@ def tipos_cursos(arbol):
 	subtipos = arbol.xpath('//subtipos/subtipo/text()')
 	subsubtipos = arbol.xpath('//subsubtipos/subsubtipo/text()')
 	for tipo in tipos:
-		if tipo not in lista:
-			lista.append(tipo)
+		lista.append(tipo)
 
 	for subtipo in subtipos:
-		if subtipo not in lista:
-			lista.append(subtipo)
+		lista.append(subtipo)
 
 	for subsubtipo in subsubtipos:
-		if subsubtipo not in lista:
-			lista.append(subsubtipo)
+		lista.append(subsubtipo)
 
-	return lista
+	return set(lista)
 
 def cantidad_curso_por_tipo(arbol):
 	lista = []
@@ -65,35 +62,51 @@ def cantidad_curso_por_tipo(arbol):
 	
 	return lista 
 
-def validador_tema(tema,arbol):
-	temas = arbol.xpath( '//curso/temas/tema/text()' )
-	if tema in temas:
-		return true
-	for i in temas:
-		if tema.upper() == i.upper():
-			return true
+def validador_cadena(cadena, arbol, tipo, texto = ' Error. Vuelva a introducirlo: '):
+	if tipo == 1:
+		listacadenas = arbol.xpath( '//curso/temas/tema/text()' )
+	if tipo == 2:
+		listacadenas = arbol.xpath( '//curso/perfiles/perfil/text()' )
+	for i in listacadenas:
+		if cadena.upper().replace('Á','A').replace('É','E').replace('Í','I').replace('Ó','O').replace('Ú','U') == i.upper().replace('Á','A').replace('É','E').replace('Í','I').replace('Ó','O').replace('Ú','U'):
+			return i
+	cadena = input(texto)
+	return validador_cadena(cadena, arbol, tipo, texto)
+
+def validador_mes(mes,arbol):
+	if mes.isdecimal():
+		if int(mes) >= 1 and int(mes) <= 12:
+			return int(mes)
+	if mes.isalpha():
+		meses=['ENERO','FEBRERO','MARZO','ABRIL','MAYO','JUNIO','JULIO','AGOSTO','SEPTIEMBRE','OCTUBRE','NOVIEMBRE','DICIEMBRE']		
+		for i in range(len(meses)):
+			if mes.upper() == meses[i]:
+				return i + 1
+	mes = input(' Mes Incorrecto\n Mes: ')
+	return validador_mes(mes,arbol)
+
 
 def menu(arbol):
 	opcion = 0
 	while (opcion != 6):
 		os.system('clear')
-		print(' 1 - Listar Cursos\n 2 - Cantidad de cursos por tema y perfil\n 3 - Buscar curso por mes de inicio\n 4 - URL Mapa del curso por tema\n 5 - Cantidad de cursos por tipos\n 6 - Salir')
+		print(' CURSOS EN ARCHIVO XML\n {}\n 1 - Listar Cursos\n 2 - Cantidad de cursos por tema y perfil\n 3 - Buscar curso por mes de inicio\n 4 - URL Mapa del curso por tema\n 5 - Cantidad de cursos por tipos\n 6 - Salir'.format('=' * 21))
 		opcion = int(input(' Opción: '))
 		if opcion == 1:
 			for i in listar_nomb_tema_fechaini(arbol):
 				print(' Nombre: {}\n Temas: {}\n Fecha Inicio: {}\n'.format(i[0],i[1],i[2]))
 		if opcion == 2:
-			tema = input(' Tema: ')
-			perfil = input(' Perfil: ')
+			tema = validador_cadena(input(' Tema: '),arbol, 1, ' Tema Incorrecto.\n Tema: ')
+			perfil = validador_cadena(input(' Perfil: '),arbol, 2, ' Perfil Incorrecto.\n Perfil: ')
 			print(' Cursos con el tema "{}" y el perfil "{}": {}'.format(tema,perfil,curso_por_tema_perfil(tema,perfil,arbol)))
 		
 		if opcion == 3:
-			mes = int(input(' Mes de Inicio: '))
+			mes = validador_mes(input(' Mes de Inicio: '),arbol)
 			for i in fecha_por_mes(mes,arbol):
 				print(' Nombre: {}\n Fecha Inicio: {}\n'.format(i[0],i[1]))
 
 		if opcion == 4:
-			tema = input(' Tema: ')
+			tema = validador_cadena(input(' Tema: '),arbol, 1, ' Tema Incorrecto.\n Tema: ')
 			for i in url_mapa(tema,arbol):
 				print(' Nombre: {}\n URL: {}\n'.format(i[0],i[1]))
 		if opcion == 5:
@@ -103,4 +116,4 @@ def menu(arbol):
 		input('\n Pulse cualquier tecla para continuar.')
 
 arbol = etree.parse('cursos.xml')
-menu(validador_tema('educacion',arbol))
+menu(arbol)
